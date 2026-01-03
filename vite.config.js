b66f8dd73191fb45d4b2707cb957a71f04a1ef34
saqlainhaider2700 <138958018+saqlainhaider27@users.noreply.github.com>
2026-01-03 17:09:07 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 👈 Add this line. It must start and end with a forward slash.
+  base: '/coffee-beans/',
 })
